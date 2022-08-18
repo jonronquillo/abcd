5748018fbc8141a2b9bb4e4fadb52a1b74beecd9
jonronquillo <111455314+jonronquillo@users.noreply.github.com>
2022-08-18 10:36:25 +0800

    Delete frappe-bench/abcd_translator directory

--- frappe-bench/abcd_translator/abcd_translator/doctype/translation_table/translation_table.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Jon Ronquillo and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class TranslationTable(Document):
-	pass
